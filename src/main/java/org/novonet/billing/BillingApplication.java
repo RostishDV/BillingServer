97bdb9c0adc9477ca2880505ac935d0eb0c7a1f2
RostishDV <rostik.drizhd@gmail.com>
2021-02-26 11:23:43 +0300

    26.02.2021

--- src/main/java/org/novonet/billing/BillingApplication.java
+++ src/main/java/org/novonet/billing/BillingApplication.java
@@ -1,13 +1,19 @@
 package org.novonet.billing;
 
+import org.novonet.billing.models.Subscriber;
+import org.novonet.billing.repo.DebitRepository;
+import org.novonet.billing.repo.SubscriberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class BillingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BillingApplication.class, args);
 	}
 
+	//todo: something to execute scheduled task of update debits
 }
