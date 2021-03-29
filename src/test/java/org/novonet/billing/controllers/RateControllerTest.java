69a29d7a495913a94af576ae06996815471b9580
RostishDV <rostik.drizhd@gmail.com>
2021-03-29 08:42:25 +0300

    addRateControllerTest

--- src/test/java/org/novonet/billing/controllers/RateControllerTest.java
+++ src/test/java/org/novonet/billing/controllers/RateControllerTest.java
@@ -10,9 +10,9 @@ import org.novonet.billing.service.RateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @TestController
 public class RateControllerTest {
@@ -22,15 +22,25 @@ public class RateControllerTest {
     @Autowired
     private RateService rateService;
 
-    private Rate testRate;
+    private Rate testRate1;
+    private Rate testRate2;
 
-    @BeforeTestClass
+    @BeforeEach
     void setUp(){
-        testRate = new Rate();
-        testRate.setPrice(100.);
-        testRate.setName("Название");
+        testRate1 = new Rate();
+        testRate1.setPrice(100.);
+        testRate1.setName("Название");
         try {
-            rateService.save(testRate) ;
+            rateService.save(testRate1) ;
+        } catch (EntryWithSameNameAlreadyExist entryWithSameNameAlreadyExist) {
+            entryWithSameNameAlreadyExist.printStackTrace();
+        }
+
+        testRate2 = new Rate();
+        testRate2.setPrice(100.);
+        testRate2.setName("Другое название");
+        try {
+            rateService.save(testRate2) ;
         } catch (EntryWithSameNameAlreadyExist entryWithSameNameAlreadyExist) {
             entryWithSameNameAlreadyExist.printStackTrace();
         }
@@ -50,10 +60,48 @@ public class RateControllerTest {
     @Test
     @DisplayName("попытка создания существующего тарифа")
     public void createExistedRate(){
-        ResponseEntity responseEntity = rateController.addNewRate(testRate.getName(), 400.);
-        assertEquals(HttpStatus.METHOD_NOT_ALLOWED, responseEntity.getStatusCode());
-        assertEquals(testRate.getName(), responseEntity.getBody());
+        ResponseEntity actualResponse = rateController.addNewRate(testRate1.getName(), 400.);
+        assertEquals(HttpStatus.METHOD_NOT_ALLOWED, actualResponse.getStatusCode());
+        assertEquals(testRate1.getName(), actualResponse.getBody());
     }
 
+    @Test
+    @DisplayName("попытка удаления существующего тарифа по ID")
+    public void deleteExistedRateById(){
+        ResponseEntity responseEntity = rateController.deleteById((long) 1);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(testRate1, responseEntity.getBody());
+    }
 
+    @Test
+    @DisplayName("Попытка удаления несуществующего тарифа по ID")
+    public void deleteNonExistedRateById(){
+        ResponseEntity responseEntity = rateController.deleteById((long) 100);
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertEquals((long) 100, responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Попытка получения существующего тарифа")
+    public void getExistedRateById(){
+        ResponseEntity responseEntity = rateController.findById((long) 2);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(testRate2, responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Попытка получения не существующего тарифа")
+    public void getNonExistedRateById(){
+        ResponseEntity responseEntity = rateController.findById((long) 100);
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+        assertEquals((long) 100, responseEntity.getBody());
+    }
+
+    @Test
+    @DisplayName("Получение всех тарифов")
+    public void getAllRates(){
+        ResponseEntity responseEntity = rateController.findAll();
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNotNull(responseEntity.getBody());
+    }
 }
